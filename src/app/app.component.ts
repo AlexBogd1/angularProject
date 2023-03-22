0cc1e4cf641e87dba5e050700c5c19cc33f62d42
Alex Bogdanovich <7894664@mail.ru>
2023-03-22 21:48:33 +0100

    add product service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './models/product';
 import { products as data } from './data/products';
+import { ProductsService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular app';
+export class AppComponent implements OnInit {
+  constructor(private productsService: ProductsService) {}
 
-  products: IProduct[] = data;
+  loading = false;
+  title = 'angular app';
+  products: IProduct[] = [];
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.productsService.getAll().subscribe((products) => {
+      this.products = products;
+      this.loading = false;
+    });
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IProduct } from '../models/product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
+
+  getAll(): Observable<IProduct[]> {
+    return this.httpClient.get<IProduct[]>(
+      'https://fakestoreapi.com/products',
+      {
+        params: new HttpParams({ fromObject: { limit: 5 } }),
+      }
+    );
+  }
 }
